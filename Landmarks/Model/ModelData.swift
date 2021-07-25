0d0ba2e01b6094788164e7446c6c32ceeb3b7606
Joss <joscelynrose@gmail.com>
2021-07-24 20:16:02 -0400

    Added a landmark row

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -10,13 +10,13 @@ import Foundation
 var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
-    
     let data: Data
-    
-    guard let file = Bundle.main.url(forResource: filename, with Extension: nil)
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
-        fatalError("Couldn't find \(filename) in main bundle."
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
+
     do {
         data = try Data(contentsOf: file)
     } catch {
